c5d679710139cb75d0a611a9ebaebdfffe67da19
ShIA-0815 <shibari0815@gmail.com>
2026-07-12 19:54:48 +0900

    テーブルの中身をリセット

--- server.js
+++ server.js
@@ -35,6 +35,9 @@ pool.connect(async (err, client, release) => {
     
     // テーブルの自動作成
     try {
+        await client.query('DROP TABLE IF EXISTS foods;');
+        console.log('dropped old table');
+
         await client.query(`
             CREATE TABLE IF NOT EXISTS foods (
                 id SERIAL PRIMARY KEY,
