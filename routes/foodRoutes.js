03b786169b4cd33ab6a4cdd6ca91fb33e2b8ab01
ShIA-0815 <shibari0815@gmail.com>
2026-08-14 22:45:00 +0900

    checkAuthの追あk

--- routes/foodRoutes.js
+++ routes/foodRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const foodController = require('../controllers/foodController');
 const upload = require('../config/cloudinary');
+const checkAuth = require('../middlewares/authMiddleware');
 
 // POST: 食材アップロード
-router.post('/upload-food', upload.single('image'), foodController.uploadFood);
+router.post('/upload-food', checkAuth, upload.single('image'), foodController.uploadFood);
 
 // GET: 食材一覧取得
 router.get('/get-foods', foodController.getFoods);
