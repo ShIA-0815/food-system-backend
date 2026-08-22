07e1d28633572d4ea18ae3493cf458aedbb5243f
ShIA-0815 <shibari0815@gmail.com>
2026-08-22 23:17:37 +0900

    lineRoutesの位置を修正

--- server.js
+++ server.js
@@ -12,13 +12,16 @@ connectDB();
 
 // ミドルウェアの設定
 app.use(cors());
+
+app.use('/api/line', lineRoutes);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // ルーティング適用 (/api/upload-food や /api/get-foods に繋がる)
 app.use('/api', foodRoutes);
-app.use('/api/line', lineRoutes);
+
 
 // PORT開放
 const PORT = process.env.PORT || 3000;
