e973a696f9cfc361aa66085df74bf7b6f21dbe81
ShIA-0815 <shibari0815@gmail.com>
2026-08-29 21:35:21 +0900

    path関連の修正

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const checkAuth = require('../middleware/checkAuth'); // トークン検証用ミドルウェア
+const checkAuth = require('../middlewares/authMiddleware');
 
 router.post('/generate-link-code', checkAuth, userController.generateLinkCode);
 
